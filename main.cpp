0df4bbbd66650e64ae3577a029cfc8953febbda1
vladg <vladgabrielairinei@yahoo.com>
2025-03-24 23:56:45 +0200

    v0.1

--- main.cpp
+++ main.cpp
@@ -42,7 +42,7 @@ public:
     bool operator==(const card &) const {
         return true;
     }
-    std::string& get_type() const  { return type; }
+    std::string& get_type() const  { return *type; }
 };
 
 
